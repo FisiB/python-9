39eab6c02d2d879e29efabb52eb8edea891b761e
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-04-29 20:48:57 +0200

    extra

--- main.py
+++ main.py
@@ -10,3 +10,22 @@ print(list_of_sum)
 list_of_max=max([1, 2, 3, 10,5])
 
 print(list_of_max)
+
+import math
+
+class Shape:
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+    
+circle=Circle(5)
+
+print("Area of shape",circle.area())
+
+        
