0eca7775ee65c239f9ffaf3d34096b66b314e5d2
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-04-29 20:31:17 +0200

    len,max,sum

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 
-def sound():
-    print("zeri")
-sound()
+list_of_len=len([1, 2, 3, 4,5])
+
+print(list_of_len)
+
+list_of_sum=sum([1,2,5,7,9])
+
+print(list_of_sum)
+
+list_of_max=max([1, 2, 3, 10,5])
+
+print(list_of_max)
